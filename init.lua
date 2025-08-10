7aaf345a31e7674d8403fa3a4b7b6bf222d9b867
MykhailoS <strepetea+github@proton.me>
2025-08-10 12:47:00 +0200

    Bound mouse scrolling to simple up and down

--- init.lua
+++ init.lua
@@ -58,6 +58,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Mouse scrolling
+vim.keymap.set('', '<ScrollWheelUp>', 'k', { noremap = true, silent = true })
+vim.keymap.set('', '<ScrollWheelDown>', 'j', { noremap = true, silent = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
