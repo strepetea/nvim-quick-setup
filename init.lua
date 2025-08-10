501b8383862f9d0eb118a56ba97affb9569fc30a
MykhailoS <strepetea+github@proton.me>
2025-08-10 14:09:41 +0200

    The toggling terminal now works!

--- init.lua
+++ init.lua
@@ -50,7 +50,7 @@ vim.api.nvim_set_keymap('n', 'q', '<nop>', { noremap = true, silent = true }) --
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 --  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
@@ -106,68 +106,74 @@ require('lazy').setup({
 
     config = function()
       require('FTerm').setup {
-        {
-          ---Filetype of the terminal buffer
-          ---@type string
-          ft = 'FTerm',
+        ---Filetype of the terminal buffer
+        ---@type string
+        ft = 'FTerm',
 
-          ---Command to run inside the terminal
-          ---NOTE: if given string[], it will skip the shell and directly executes the command
-          ---@type fun():(string|string[])|string|string[]
-          -- cmd = os.getenv 'SHELL',
+        ---Command to run inside the terminal
+        ---NOTE: if given string[], it will skip the shell and directly executes the command
+        ---@type fun():(string|string[])|string|string[]
+        cmd = nil,
 
-          ---Neovim's native window border. See `:h nvim_open_win` for more configuration options.
-          border = 'rounded',
+        ---Neovim's native window border. See `:h nvim_open_win` for more configuration options.
+        border = 'rounded',
 
-          ---Close the terminal as soon as shell/command exits.
-          ---Disabling this will mimic the native terminal behaviour.
-          ---@type boolean
-          auto_close = true,
+        ---Close the terminal as soon as shell/command exits.
+        ---Disabling this will mimic the native terminal behaviour.
+        ---@type boolean
+        auto_close = false,
 
-          ---Highlight group for the terminal. See `:h winhl`
-          ---@type string
-          hl = 'Normal',
+        ---Highlight group for the terminal. See `:h winhl`
+        ---@type string
+        hl = 'Normal',
 
-          ---Transparency of the floating window. See `:h winblend`
-          ---@type integer
-          blend = 0,
+        ---Transparency of the floating window. See `:h winblend`
+        ---@type integer
+        blend = 0,
 
-          ---Object containing the terminal window dimensions.
-          ---The value for each field should be between `0` and `1`
-          ---@type table<string,number>
-          dimensions = {
-            height = 0.8, -- Height of the terminal window
-            width = 0.8, -- Width of the terminal window
-            x = 0.5, -- X axis of the terminal window
-            y = 0.5, -- Y axis of the terminal window
-          },
-
-          ---Replace instead of extend the current environment with `env`.
-          ---See `:h jobstart-options`
-          ---@type boolean
-          clear_env = false,
-
-          ---Map of environment variables extending the current environment.
-          ---See `:h jobstart-options`
-          ---@type table<string,string>|nil
-          env = nil,
-
-          ---Callback invoked when the terminal exits.
-          ---See `:h jobstart-options`
-          ---@type fun()|nil
-          on_exit = nil,
-
-          ---Callback invoked when the terminal emits stdout data.
-          ---See `:h jobstart-options`
-          ---@type fun()|nil
-          on_stdout = nil,
-
-          ---Callback invoked when the terminal emits stderr data.
-          ---See `:h jobstart-options`
-          ---@type fun()|nil
-          on_stderr = nil,
+        ---Object containing the terminal window dimensions.
+        ---The value for each field should be between `0` and `1`
+        ---@type table<string,number>
+        dimensions = {
+          height = 0.8, -- Height of the terminal window
+          width = 0.8, -- Width of the terminal window
+          x = 0.5, -- X axis of the terminal window
+          y = 0.5, -- Y axis of the terminal window
         },
+
+        ---Replace instead of extend the current environment with `env`.
+        ---See `:h jobstart-options`
+        ---@type boolean
+        clear_env = false,
+
+        ---Map of environment variables extending the current environment.
+        ---See `:h jobstart-options`
+        ---@type table<string,string>|nil
+        env = nil,
+
+        ---Callback invoked when the terminal exits.
+        ---See `:h jobstart-options`
+        ---@type fun()|nil
+        on_exit = nil,
+
+        ---Callback invoked when the terminal emits stdout data.
+        ---See `:h jobstart-options`
+        ---@type fun()|nil
+        on_stdout = nil,
+
+        ---Callback invoked when the terminal emits stderr data.
+        ---See `:h jobstart-options`
+        ---@type fun()|nil
+        on_stderr = nil,
       }
+      vim.api.nvim_create_user_command('FTermOpen', require('FTerm').open, { bang = true })
+      vim.api.nvim_create_user_command('FTermClose', require('FTerm').close, { bang = true })
+      vim.api.nvim_create_user_command('FTermExit', require('FTerm').exit, { bang = true })
+      vim.api.nvim_create_user_command('FTermToggle', require('FTerm').toggle, { bang = true })
+
+      vim.keymap.set('n', '<leader>tt', '<cmd>FTermToggle<CR>', { desc = '[T]oggle [T]erminal' })
+      vim.keymap.set('t', '<Esc><Esc>', '<cmd>FTermToggle<CR>')
+      vim.keymap.set('t', '<C-q>', [[<cmd>FTermExit<CR>]])
     end,
   },
 
